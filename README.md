5f80bd0ed821dec0ddb0264f2b734b404e41380c
Yair Even Or <vsync.design@gmail.com>
2024-04-21 19:53:09 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Tagify Test App
+
+A basic React App (Vite + SWC) for testing my [Tagify package](https://github.com/yairEO/tagify)
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import '@yaireo/tagify/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>123</>
+)
